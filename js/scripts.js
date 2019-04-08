b0485224f6f5d2255b88beb0cd35b184d86f0cb6
EllieH <ellie.homan@hotmail.com>
2019-04-08 18:50:29 +0100

    Updated Checkbox Content
    - removed duplicates of selection buttons
    - turned list into a <ul> giving bullet points
    - Updated so that when you uncheck a box, item is removed from list not duplicated

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
-$(document).ready(function() {
-  $( "input[type='checkbox']" ).change(function() {
-    $("#ingredients").append("<div>" + $(this).val() + "</div>");
+$(document).ready(function(){
+  $('input[type="checkbox"]').click(function(){
+    var li_text = $(this).next().html();
+    if($(this).is(":checked")){
+      $("#ingredientslist").append('<li id="select'+this.id+'">'+li_text+'</li>');
+    } else {
+      $("#select"+this.id).remove();
+    }
   });
 });
